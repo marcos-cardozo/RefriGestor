4f76d15ced22ad81751acfc6d846059cac2d2d5c
marcos <marcosnazarenocardozo@gmail.com>
2026-05-13 08:39:41 +0200

    fix: update cors config

--- refrilog-backend/src/main.ts
+++ refrilog-backend/src/main.ts
@@ -9,6 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:3000', 'https://refri-gestor.vercel.app'],
+
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+
+    credentials: true,
   });
   app.useGlobalPipes(
     new ValidationPipe({
