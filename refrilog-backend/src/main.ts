4f5a806ffd8acae9e65415857681dff5312207c3
marcos <marcosnazarenocardozo@gmail.com>
2026-05-13 08:35:13 +0200

    back error

--- refrilog-backend/src/main.ts
+++ refrilog-backend/src/main.ts
@@ -8,7 +8,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://refri-gestor.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://refri-gestor.vercel.app'],
   });
   app.useGlobalPipes(
     new ValidationPipe({
